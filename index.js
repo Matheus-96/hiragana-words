49bd306c0b3ec135d0d231d9032e5218673288ed
Victor Matheus <victor.id96@hotmail.com>
2021-07-23 15:47:16 -0300

    COMMIT

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const axios = require('axios').default;
-
+const axios = require('axios').default
+const cheerio = require('cheerio')
 
 //mongoose.connect('mongodb')
 const app = express();
 
-app.get('/:query', function(req, res){
-    
+app.get('/:query', async function(req, res){
     
     // ### PYTHON - BAD USE FOR NODE/HEROKU ###
     // const {spawn} = require('child_process')
@@ -16,28 +15,51 @@ app.get('/:query', function(req, res){
     //     res.header('Content-Type', 'application/json')
     //     res.send(data);
     // })
+        
 
-
-    axios({
-        method: 'GET',
-        url: 'https://jisho.org/search/ki',
-        responseType: 'text'
-    })
-    .then((response)=>{
-
-        res.send(response.data)
-    })
-    .then((error)=>{
-
-    })
-    .then(function(){
-        console.log("teste")
-    })
-
- 
+    counter = 0;
+    numOfPages = 1
+    myData = []
+    console.log("iniciou")
+    await getData(req.params.query)
+    res.send(myData)
+    console.log("finalizou")
 })
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Our app is running on port ${ PORT }`);
 });
+
+async function getData(query, newUrl = 0){
+    await axios({
+        method: 'GET',
+        url: (newUrl!=0 ? newUrl : 'https://jisho.org/search/'+query+"%20%23words"),
+        responseType: 'text'
+    })
+    .then((response)=>{
+
+        counter = counter + 1
+        html = response.data
+        $ = cheerio.load(html)
+        //concepts = cheerio.load(soup.html('div .concepts'))
+        $('div .concept_light').each(function(x, elem){
+            furigana = $('.furigana',elem).first().text()
+            furigana = furigana.replace(/\s+/g, ' ').trim()
+            translation = $('.meaning-meaning',elem).first().text()
+            translation = translation.replace(/\s+/g, ' ').trim()
+            if(translation && furigana) myData.push({hiragana: furigana, meaning: translation})
+        })
+        newUrl = "https:"+($('.more').attr('href'))
+        //console.log(myData)
+
+        if(counter > numOfPages){
+            console.log("teste")
+            return myData
+        } 
+
+        getData(query, newUrl)	
+    })
+
+ 
+}
