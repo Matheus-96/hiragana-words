fb0ab9194d25eab6fd96c9a88afda9d90d1f3157
Victor Matheus <victor.id96@hotmail.com>
2021-10-01 17:38:02 -0300

    Alteraçoes no web scraping do site JISHO, trazendo informaçoes consistentes juntamente com o significado de cada palavra

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 const express = require('express')
 const cors = require('cors')
 const axios = require('axios').default
-const cheerio = require('cheerio')
-
+const cheerio = require('cheerio');
+const { text } = require('body-parser');
 const app = express();
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+const PORT = process.env.PORT || 8000;
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 
-app.get('/:query', async function(req, res){
+
+app.get('/:query?', async function(req, res) {
     counter = 0;
     numOfPages = 5
     myData = []
     baseUrl = "https://jisho.org/search/"
-    await getData(req.params.query, Math.ceil(Math.random()*15))
+    await getData(req.params.query || 'any', Math.ceil(Math.random() * 15))
     res.header('Content-Type', 'application/json')
-    res.send(myData)
+    res.status(201).send(myData)
 })
 
-app.post('/words', async function(req, res){
+app.post('/words', async function(req, res) {
     counter = 0;
     numOfPages = 3
     myData = []
@@ -27,25 +29,26 @@ app.post('/words', async function(req, res){
     let reqHiragana = ""
     abc = ""
     body = req.body.hiragana.split('/')
-    
+
     body.forEach(element => {
         reqHiragana += (element.split(':')[0])
         abc += ((element.split(':')[1]) == undefined ? '' : (element.split(':')[1]))
     });
 
-    abc = abc.substr(0,abc.length-1)
+    abc = abc.substr(0, abc.length - 1)
     await getDataFiltered(reqHiragana, abc)
     res.header('Content-Type', 'application/json')
     res.send(myData)
 })
 
 //JISHO
-/*
-async function getDataFilteredJisho(filter, abcArr){
+
+async function getDataFilteredJisho(filter, abcArr) {
     abcArr = abcArr.toString().split(',')
     query = abcArr[Math.floor(Math.random() * abcArr.length)]
-    url = baseUrl+query+"%20%23words?page="+(newUrl+1)
-    
+    url = baseUrl + query + "%20%23words?page=" + (newUrl + 1)
+
+    /*
     await axios({
         method: 'GET',
         url:url,
@@ -83,86 +86,97 @@ async function getDataFilteredJisho(filter, abcArr){
         } 
         await getDataFiltered(filter, abcArr, Math.ceil(Math.random()*15))	
     })
+    */
     return
 }
-*/
 
-async function getDataFiltered(filter, abcArr, newUrl = 0){
+
+async function getDataFiltered(filter, abcArr, newUrl = 0) {
     abcArr = abcArr.toString().split(',')
     query = abcArr[Math.floor(Math.random() * abcArr.length)]
-    //url = baseUrl+query+"%20%23words?page="+(newUrl+1)
-    url = "http://www.edrdg.org/jmdictdb/cgi-bin/srchres.py?s1=1&y1=2&t1="+query+"&src=2&search=Search&svc=jmdict&p1="+Math.floor(Math.random() * 10)
+        //url = baseUrl+query+"%20%23words?page="+(newUrl+1)
+    url = "http://www.edrdg.org/jmdictdb/cgi-bin/srchres.py?s1=1&y1=2&t1=" + query + "&src=2&search=Search&svc=jmdict&p1=" + Math.floor(Math.random() * 10)
     await axios({
-        method: 'GET',
-        url:url,
-        responseType: 'text'
-    })
-    .then(async (response)=>{
-        html = response.data
-        $ = cheerio.load(html)
-        console.log(url)
-        var hiragana = ""
-        $('.resrow').each(function(x, elem){
-            
-            console.log()
-            wordOk = true
-            
-            hiragana = $('.rdng', elem).text()
-            wordOk = myData.map((e)=>{return e.hiragana}).indexOf(hiragana) == -1 ? true : false
-            
-            if(wordOk){
-                x = hiragana.length
-                while(x--){
-                    wordOk = hiragana[x] == " " ? true : filter.includes(hiragana[x])
-                    if(!wordOk) break
-                }
-                romaji = $('.gloss',elem).text()
-            }
-            if(wordOk) myData.push({hiragana: hiragana, meaning: romaji})
+            method: 'GET',
+            url: url,
+            responseType: 'text'
         })
-    })
-    
-    
+        .then(async(response) => {
+            html = response.data
+            $ = cheerio.load(html)
+            console.log(url)
+            var hiragana = ""
+            $('.resrow').each(function(x, elem) {
+
+                console.log()
+                wordOk = true
+
+                hiragana = $('.rdng', elem).text()
+                wordOk = myData.map((e) => { return e.hiragana }).indexOf(hiragana) == -1 ? true : false
+
+                if (wordOk) {
+                    x = hiragana.length
+                    while (x--) {
+                        wordOk = hiragana[x] == " " ? true : filter.includes(hiragana[x])
+                        if (!wordOk) break
+                    }
+                    romaji = $('.gloss', elem).text()
+                }
+                if (wordOk) myData.push({ hiragana: hiragana, meaning: romaji })
+            })
+        })
+
+
     console.log(myData.length)
-    if(myData.length >= 40){
+    if (myData.length >= 40) {
         return
-    } 
+    }
     await getDataFiltered(filter, abcArr)
-    
+
     return
 }
 
 
-async function getData(query, newUrl = 0){
+async function getData(query, newUrl = 0) {
     await axios({
-        method: 'GET',
-        url: (newUrl!=0 ? baseUrl+query+"%20%23words" + "?page="+newUrl : baseUrl+query+"%20%23words"),
-        responseType: 'text'
-    })
-    .then((response)=>{
-        counter = counter + 1
-        html = response.data
-        $ = cheerio.load(html)
-        //concepts = cheerio.load(soup.html('div .concepts'))
-        $('div .concepts .concept_light').each(function(x, elem){
-            furigana = $('.furigana',elem).first().text()
-
-            furigana = furigana.replace(/\s+/g, ' ').trim()
-            translation = $('.meaning-meaning',elem).first().text()
-            translation = translation.replace(/\s+/g, ' ').trim()
-            if(translation && furigana && furigana.length > 1) myData.push({hiragana: furigana, meaning: translation})
+            method: 'GET',
+            url: (newUrl != 0 ? baseUrl + query + "%20%23words" + "?page=" + newUrl : baseUrl + query + "%20%23words"),
+            responseType: 'text'
         })
-        newUrl = "https:"+($('.more').attr('href'))
+        .then((response) => {
+            counter = counter + 1
+            html = response.data
+            $ = cheerio.load(html)
+                //concepts = cheerio.load(soup.html('div .concepts'))
 
-        if(counter > numOfPages) return myData 
+            $('div .concepts .concept_light').each(function(x, elem) {
+                word = { furigana: [], text: [], meaning: '' }
 
-        getData(query, Math.round(Math.random()*15))	
-    })
+                //Adicionando cada conjunto de caracteres FURIGANA da palavra
+                $('.furigana span', elem).each((x, elem) => {
+                    word.furigana.push($(elem).text())
+                })
 
-    
+                //Pega o texto, remove espaços em branco, e separa cada caractere em um array
+                word.text = $('.text', elem).text().trim().split('')
+
+                //Pegando tradução da palavra
+                translation = $('.meaning-meaning', elem).first().text()
+                word.meaning = translation.replace(/\s+/g, ' ').trim()
+
+
+                //Preenchendo as informações da palavra no array de objetos
+                myData.push(word)
+            })
+            newUrl = "https:" + ($('.more').attr('href'))
+
+            if (counter > numOfPages) return myData
+
+            getData(query, Math.round(Math.random() * 15))
+        })
+
 }
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Our app is running on port ${ PORT }`);
 });
